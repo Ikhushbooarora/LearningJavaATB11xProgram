78a63d69a4636b418ce0959181f58908ba550aad
Administrator <arorakhushboo09@gmail.com>
2025-03-11 00:33:15 +0530

    10th March

---
+++ src/ex_01_Java_Basic/Lab002_comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab002_comments {
+    public static void  main(String[] args){
+
+        System.out.println("Hello World");
+//        This is a comment only
+    }
+}
