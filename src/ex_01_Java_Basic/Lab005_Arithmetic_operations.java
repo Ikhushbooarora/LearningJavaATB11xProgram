cc542141511c1060cdf3faf5ea908254ee678b58
Administrator <arorakhushboo09@gmail.com>
2025-03-11 01:18:13 +0530

    10th March

---
+++ src/ex_01_Java_Basic/Lab005_Arithmetic_operations.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Arithmetic_operations {
+    public static void main(String[] args){
+
+
+        System.out.println(5+5);
+        System.out.println(5*5);
+        System.out.println(6/3);
+        System.out.println(4-2);
+        System.out.println(4/3);
+    }
+}
