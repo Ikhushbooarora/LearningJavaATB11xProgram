240343a317ee99f7e38e030b63ec256d83863d0c
Administrator <arorakhushboo09@gmail.com>
2025-03-11 00:37:33 +0530

    10th March

---
+++ src/ex_01_Java_Basic/Lab003_multiplecomments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+//this is a comment
+public class Lab003_multiplecomments {
+//    this is also a comment
+    public static void main(String[] args){
+//        This is another comment we dont have to do anything
+        System.out.println("HI");
+    }
+}
