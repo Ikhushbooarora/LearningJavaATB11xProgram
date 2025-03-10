b5d69df0cf712daf5a0397e5279aca3550750dd8
Administrator <arorakhushboo09@gmail.com>
2025-03-11 00:53:45 +0530

    10th March

---
+++ src/ex_01_Java_Basic/Lab004_Comments_Shortcuts.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments_Shortcuts {
+    public static void main(String[] args){
+
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
