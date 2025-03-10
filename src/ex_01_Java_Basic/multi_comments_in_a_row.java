33e3181871187713cfa33468c92ac327fa2f1707
Administrator <arorakhushboo09@gmail.com>
2025-03-11 00:56:00 +0530

    10th March

---
+++ src/ex_01_Java_Basic/multi_comments_in_a_row.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class multi_comments_in_a_row {
+    public static void main(String[] args){
+        /*
+         * Khushboo is my name
+         * I am learning java and web automation
+         *I am learning git too
+         */
+        System.out.println();
+
+    }
+}
