04ea4e93fd3dfcfd772e1847dc0f5ecd44af38b8
Administrator <arorakhushboo09@gmail.com>
2025-03-12 16:48:40 +0530

    12th March

---
+++ src/ex_02_Java_Basics_part2/Lab002_Literals.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab002_Literals {
+    public static void main(String[] args){
+//       This is an integer literal and can be change
+        int age = 60;
+        age = 66;
+        System.out.println(age);
+
+        final int girl_age =20;
+//                girl_age = 18; this is constant thats why it can not be changed.
+                System.out.println(girl_age);
+
+    }
+}
