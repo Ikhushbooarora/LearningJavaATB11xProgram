d490409eaf4589789ef42a65c34db6798257ce9c
Administrator <arorakhushboo09@gmail.com>
2025-03-12 17:09:27 +0530

    12th March

---
+++ src/ex_02_Java_Basics_part2/Lab004_Boolean_literals.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab004_Boolean_literals {
+    public static void main (String[] args) {
+        boolean is_married = true;
+        boolean is_not_married = false;
+    }
+    }
