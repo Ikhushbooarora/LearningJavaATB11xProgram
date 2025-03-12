1bc601a0301f6a9fa1dd31b4c532041c1995bc2f
Administrator <arorakhushboo09@gmail.com>
2025-03-12 16:40:51 +0530

    11th March

---
+++ src/ex_02_Java_Basics_part2/Lab001_Final_Example.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab001_Final_Example {
+    public static void main (String[] args){
+        int local=10;
+        System.out.println(local);
+
+        final float PI=3.14F;
+//        PI=3.14F; THIS IS FINAL IN NATURE.
+        System.out.println(PI);
+    }
+}
