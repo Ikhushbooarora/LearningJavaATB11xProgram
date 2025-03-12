d044d31874acc57f07367a55633405130ef043fa
Administrator <arorakhushboo09@gmail.com>
2025-03-12 17:12:40 +0530

    12th March

---
+++ src/ex_02_Java_Basics_part2/Lab_interview_question.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab_interview_question {
+    public static void main(String[] args){
+        int Enum = 9;
+        System.out.println(Enum);
+//        is this is a valid syntax or not ?
+    }
+}
