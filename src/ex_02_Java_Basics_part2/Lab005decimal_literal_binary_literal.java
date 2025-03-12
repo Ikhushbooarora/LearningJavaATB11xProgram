acd44ffd9cb07b8b04d353c90efc63889aaac69d
Administrator <arorakhushboo09@gmail.com>
2025-03-12 17:32:24 +0530

    12th March

---
+++ src/ex_02_Java_Basics_part2/Lab005decimal_literal_binary_literal.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab005decimal_literal_binary_literal {
+    public static void main (String[] args){
+
+    // Binary Literal
+    // Decimal System - 10 in binary 0b is used before the digit
+    int binary = 0b1010;
+
+    // Octal base? -> 8
+    int octal = 0101;
+
+
+    // Hexadecimal Literals - 16
+    int hex = 0Xface;
+    //base 16 color combination are in hex - #28a745
+//        In hex 0X is used before digit
+}
+}
