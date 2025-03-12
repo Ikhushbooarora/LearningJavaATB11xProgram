e7113211e4fe3bdc453b342453aa95c0edea5b86
Administrator <arorakhushboo09@gmail.com>
2025-03-12 16:58:54 +0530

    12th March

---
+++ src/ex_02_Java_Basics_part2/Lab003_floating_literals.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab003_floating_literals {
+    public static void main (String[] args){
+        float pie = 3.14f;
+        float pie2 = 3.14F;
+//        after 3.14 we can use either capital F or small f no issues.
+        float x = 10.00f;
+
+
+    }
+}
