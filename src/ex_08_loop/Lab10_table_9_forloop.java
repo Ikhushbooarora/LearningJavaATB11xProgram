1912c0c29687750ff3d94acbcb5d7d5d4a38556a
Administrator <arorakhushboo09@gmail.com>
2025-03-22 00:26:08 +0530

    22nd march tasks

--- src/ex_08_loop/Lab10_table_9_forloop.java
+++ src/ex_08_loop/Lab10_table_9_forloop.java
@@ -2,9 +2,9 @@ package ex_08_loop;
 
 public class Lab10_table_9_forloop {
     public static void main(String[] args) {
-       int number = 9;
-        for(int i=1;i<=10;i++){
-            System.out.println(number + "*" + i + " = " + (number*i));
+        int number = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + "*" + i + " = " + (number * i));
         }
     }
 }
